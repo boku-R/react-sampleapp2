0cdc506290ceda3379f2ce3bd196c6f8102c09e5
boku-R <117156560+boku-R@users.noreply.github.com>
2023-03-15 16:35:08 +0900

    デバッグの方法を学習

--- 08_other_function/src/060_practice_ref/start/Example.js
+++ 08_other_function/src/060_practice_ref/start/Example.js
@@ -1,15 +1,15 @@
 import { useState, useRef, forwardRef, useImperativeHandle } from "react";
 
 const Video = forwardRef(({ path }, ref) => {
-  const videoRef = useRef()
+  const videoRef = useRef();
 
   useImperativeHandle(ref, () => ({
-    play(){
-videoRef.current.play()
+    play() {
+      videoRef.current.play();
     },
-    stop(){
-videoRef.current.pause()
-    }
+    stop() {
+      videoRef.current.pause();
+    },
   }));
 
   return (
@@ -27,15 +27,16 @@ const Example = () => {
   return (
     <div>
       <h3>練習問題</h3>
-      <p>useRef、useImperativeHandle、forwardRefを使って完成系の動画再生機能を作成してください。※useImperativeHandleでplay(再生)、stop(停止)メソッドを定義すること。
-</p>
+      <p>
+        useRef、useImperativeHandle、forwardRefを使って完成系の動画再生機能を作成してください。※useImperativeHandleでplay(再生)、stop(停止)メソッドを定義すること。
+      </p>
       <Video ref={ref} path="./sample.mp4" />
       <button
         onClick={() => {
-          if(playing){
-            ref.current.stop()
-          }else{
-            ref.current.play()
+          if (playing) {
+            ref.current.stop();
+          } else {
+            ref.current.play();
           }
           setPlaying((prev) => !prev);
         }}
