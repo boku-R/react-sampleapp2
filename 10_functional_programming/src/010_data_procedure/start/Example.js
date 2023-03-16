f7fbb6e1c67ae1edbc32ae25ee79638e0aa84e7e
boku-R <117156560+boku-R@users.noreply.github.com>
2023-03-16 17:50:36 +0900

    関数型プログラミングに関する知識について学習

--- 10_functional_programming/src/010_data_procedure/start/Example.js
+++ 10_functional_programming/src/010_data_procedure/start/Example.js
@@ -1,8 +1,36 @@
 const Example = () => {
+  // 関数型
+  const nums = [1, 2, 3];
+  const sum = (arr) => {
+    const nums = arr;
+    // let sumValue = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //   sumValue += nums[i];
+    // }
+    // nums.forEach((num) => (sumValue += num)); 
+    let sumValue = nums.reduce((accu, curr)=> accu + curr);
+    return sumValue;
+  };
+
+  // オブジェクト指向型
+  const numObj = {
+    nums: [1, 2, 3],
+    sum() {
+      const nums = this.nums;
+      let sumValue = 0;
+      for (let i = 0; i < nums.length; i++) {
+        sumValue += nums[i];
+      }
+      return sumValue;
+    },
+  };
+
   return (
     <>
       <h3>状態管理と処理を分離</h3>
       <p>状態（データ）と処理（やりたいこと）は切り離す</p>
+      <p>オブジェクト思考型:{numObj.sum()}</p>
+      <p>関数型:{sum(nums)}</p>
     </>
   );
 };
