b3c2b82e6f54e07fe55494c33fada6e0d19b1dfb
boku-R <117156560+boku-R@users.noreply.github.com>
2023-03-20 18:41:45 +0900

    useEffectとcustomHookについて学習

--- 12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
+++ 12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
@@ -1,18 +1,40 @@
 import { useEffect, useState } from "react";
 
 const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
+
+  return (
+    <>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp((prev) => !prev)}>トグル</button>
+    </>
+  );
+};
+
+const Timer = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
-    window.setInterval(() => {
-      setTime(prev => prev + 1);
+    // console.log('init');
+    let intervalId = null;
+    intervalId = window.setInterval(() => {
+      console.log('interval called');
+      setTime((prev) => prev + 1);
     }, 1000);
-  }, [])
-  
+    return () => {
+      window.clearInterval(intervalId)
+      // console.log("end");
+    };
+  }, []);
+
   useEffect(() => {
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
+    // console.log('updated');
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key-end", time);
+
+    return () => {
+      // console.log('update end');
+    }
   }, [time]);
 
   return (
@@ -20,7 +42,7 @@ const Example = () => {
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
-    );
+  );
 };
 
 export default Example;
