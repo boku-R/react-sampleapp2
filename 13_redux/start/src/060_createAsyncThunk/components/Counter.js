0d29381d8021403b743a79b1d2bbfdb5d3f8dc61
boku-R <117156560+boku-R@users.noreply.github.com>
2023-04-11 17:41:23 +0900

    学習途中

--- 13_redux/start/src/060_createAsyncThunk/components/Counter.js
+++ 13_redux/start/src/060_createAsyncThunk/components/Counter.js
@@ -1,16 +1,61 @@
-import { add, minus,addAsync } from "../store/modules/counter"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import CounterResult from "./CounterResult"
-import CounterButton from "./CounterButton"
+import { asyncCount } from "../../api/counter"
 
-const Counter = () => {
-    return (
-        <>
-            <CounterResult />
-            <CounterButton step={2} calcType="+" actionCreator={add}/>
-            <CounterButton step={2} calcType="-" actionCreator={minus}/>
-            <CounterButton step={2} calcType="非同期(+)" actionCreator={addAsync}/>
-        </>
-    )
+const counter = createSlice({
+  name: 'counter',
+  initialState: {
+    count: 0,
+    status: ''
+  },
+  reducers: {
+    add(state, { type, payload }) {
+      state.count = state.count + payload;
+      // const newState = { ...state };
+      // newState.count = state.count + payload
+      // return newState;
+    },
+    minus(state, { type, payload }) {
+      state.count = state.count - payload;
+      // const newState = { ...state };
+      // newState.count = state.count - payload
+      // return newState;
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addAsyncWithStatus.pending, (state) => {
+      state.status = 'Loading...'
+    })
+    .addCase(addAsyncWithStatus.fulfilled, (state, action) => {
+      state.status = '取得済'
+      state.count = state.count + action.payload;
+    })
+    .addCase(addAsyncWithStatus.rejected, (state) => {
+      state.status = 'エラー'
+    })
+    
+  }
+});
+
+const { add, minus } = counter.actions;
+
+const addAsyncWithStatus = createAsyncThunk(
+  'counter/asyncCount',
+  async (payload) => {
+    const response = await asyncCount(payload);
+    return response.data;
+  }
+)
+
+const addAsync = (payload) => {
+  return async (dispatch, getState) => {
+    const state = getState();
+    console.log(state);
+    const response = await asyncCount(payload);
+    dispatch(add(response.data));
+  }
 }
-export default Counter;
+
+
+export { add, minus, addAsync, addAsyncWithStatus }
+export default counter.reducer
